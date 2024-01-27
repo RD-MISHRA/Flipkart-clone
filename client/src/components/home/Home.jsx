548be83d68a7081e46466189cdaa3c959edba782
RD-MISHRA <125600380+RD-MISHRA@users.noreply.github.com>
2024-01-27 23:30:42 +0530

    used redux to get the products on frontend

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,13 +3,24 @@ import { Fragment } from "react";
 import NavBar from "./NavBar";
 import Banner from './Banner';
 import {styled,Box} from '@mui/material';
-
+import { useEffect } from "react";
+import { getProducts } from "../../redux/actions/productActions";
+import {useDispatch,useSelector} from 'react-redux';
 const Component=styled(Box)`
 padding:10px;
 background:#F2F2F2;
 `
 
 const Home=()=>{
+    //getProducts is state here
+const {products}=useSelector(state=>state.getProducts)
+console.log(products);
+    const dispatch=useDispatch();
+useEffect(()=>{
+    dispatch(getProducts())
+
+},[dispatch])
+
     return (
         <Fragment><NavBar/>
        
